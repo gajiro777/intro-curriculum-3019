bcaa5323403928f5d15b5e0d59c2b36f1b246e96
gajiro777 <ga-ji_@cb3.so-net.ne.jp>
2017-10-23 11:17:06 +0000

    リダイレクトの実装

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location' : 'https://www.nhk.or.jp/radio/?area=osaka'
+  });
   res.end();
 });
 const port = 8000;
